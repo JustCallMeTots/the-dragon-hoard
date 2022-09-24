4ffa42aca0d23a2ec4fa6d36b9af1e78153a40bc
Micah Stotsky <Leowyatt09@gmail.com>
2022-09-24 08:43:25 -0500

    netlify

--- pages/myCharacters.js
+++ pages/myCharacters.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { getCharactersByUid } from '../api/characterData';
 import CharacterCard from '../components/CharacterCard';
